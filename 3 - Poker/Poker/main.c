df65073ea5824c5d533058efd51887ffebf94450
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2013-12-02 23:47:54 -0200

    Updates on Poker exercise

--- 3 - Poker/Poker/main.c
+++ 3 - Poker/Poker/main.c
@@ -7,25 +7,73 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 
 FILE *file;
 char path[] = "/Users/rafaelks/Projects/BEPiD/3 - Poker/Poker/poker.txt";
-int counter1 = 0;
-int counter2 = 0;
-
 char line[30];
-char *cards[1000][2];
 
-char *caracter;
+
+int getWinner(char *player1[5], char *player2[5]) {
+	int i;
+	int j;
+	
+	printf("PLAYER 1\n");
+	for (i = 0; i < 5; i++) {
+		printf("%s", player1[i]);
+	}
+	
+	
+	printf("\n\nPLAYER 2\n");
+	for (j = 0; j < 5; j++) {
+		printf("%s", player2[j]);
+	}
+	
+	printf("\n");
+
+	return 1;
+}
+
 
 int main(int argc, const char * argv[])
 {
     file = fopen(path, "r");
     
-    while(fgets(line, 29, file) != NULL) {
-        for (int i = 0; i < 15; i++) {
-            
-        }
+	// RKS NOTE: Get file line by line
+    while(fgets(line, 30, file) != NULL) {
+        char *cards = strtok(line, " ");
+		char *player1[5];
+		char *player2[5];
+		int counter = 1;
+		int cardCounter = 0;
+
+		// RKS NOTE: Start reading first card
+		player1[cardCounter] = cards;
+		printf("Player 1: %s\n", cards);
+		
+		// RKS NOTE: Let's separate who plays WTF
+		while (cards != NULL) {
+			cards = strtok(NULL, " ");
+			
+			if (cards != NULL) {
+				if ((counter % 2) == 0) {
+					printf("Player 1: %s\n", cards);
+					player1[cardCounter] = cards;
+				} else {
+					printf("Player 2: %s\n", cards);
+					player2[cardCounter] = cards;
+				
+					// RKS NOTE: Whe change user, we must increase
+					// cardCounter value
+					cardCounter++;
+				}
+			}
+			
+			counter++;
+		}
+		
+		// RKS NOTE: Calculate who wins!
+		printf("Player %i wins!\n\n\n", getWinner(player1, player2));
     }
 }
 
