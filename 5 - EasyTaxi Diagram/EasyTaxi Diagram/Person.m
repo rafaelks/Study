ec906ff1e3821f8063b2f06458a5ae3983fed132
Txai Wieser <txaidw@gmail.com>
2013-12-05 11:30:52 -0200

    Add Classes Person, Point, point not finished

---
+++ 5 - EasyTaxi Diagram/EasyTaxi Diagram/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  EasyTaxi Diagram
+//
+//  Created by Txai Wieser on 05/12/13.
+//  Copyright (c) 2013 Rafael Kellermann Streit. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
