2f562001182e1479980a1073f4b5ffe8efb6f5f8
fabiocampero <fabiocampero@gmail.com>
2013-12-06 10:36:29 -0200

    Add Run.h & Run.m

--- 5 - EasyTaxi Diagram/EasyTaxi Diagram/Run.h
+++ 5 - EasyTaxi Diagram/EasyTaxi Diagram/Run.h
@@ -10,4 +10,18 @@
 
 @interface Run : NSObject
 
+- (void)setDriver:(Driver *)driver;
+- (void)setPerson:(Person *)person;
+- (void)setDistance:(float)distance;
+- (void)setDate:(NSDate *)date;
+- (void)setRate:(NSInteger *)rate;
+- (void)setPayment:(Payment *)payment;
+
+- (Driver *)getDriver;
+- (Person *)getPerson;
+- (float)getDistance;
+- (NSDate *)getDate;
+- (NSInteger *)getRate;
+- (Payment *)getPayment;
+
 @end
--- 5 - EasyTaxi Diagram/EasyTaxi Diagram/Run.m
+++ 5 - EasyTaxi Diagram/EasyTaxi Diagram/Run.m
@@ -7,8 +7,89 @@
 //
 
 #import "Run.h"
+#import "Payment.h"
+#import "Driver.h"
+#import "Person.h"
 
 @implementation Run
+{
+    Driver    *_driver;
+    Person    *_person;
+    Payment   *_payment;
+    float     _distance;
+    float     _price;
+    NSInteger *_rate;
+    NSDate    *_date;
     
+}
+
+- (void)setDriver:(Driver *)driver
+{
+    _driver = driver;
+}
+
+- (void)setPerson:(Person *)person
+{
+    _person = person;
+}
+
+- (void)setPayment:(Payment *)payment
+{
+    _payment = payment;
+}
+
+- (void)setDistance:(float)distance
+{
+    _distance = distance;
+}
+
+- (void)setPrice:(float)price
+{
+    _price = price;
+}
+
+- (void)setRate:(NSInteger *)rate
+{
+    _rate = rate;
+}
+- (void)setDate:(NSDate *)date
+{
+    _date = date;
+}
+
+- (Driver *)getDriver
+{
+    return _driver;
+}
+
+- (Person *)getPerson
+{
+    return _person;
+}
+
+- (Payment *)getPayment
+{
+    return _payment;
+}
+
+- (float)getDistance
+{
+    return _distance;
+}
+
+- (float)getPrice
+{
+    return _price;
+}
+
+- (NSInteger *)getRate
+{
+    return _rate;
+}
+
+- (NSDate *)getDate
+{
+    retuen _date;
+}
 
 @end
