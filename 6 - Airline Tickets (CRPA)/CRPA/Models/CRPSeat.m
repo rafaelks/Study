5cf159abad385c936a571e0f9306a16c0e0c8c0f
Filipe Alvarenga <filipealvarenga@MacBook-Pro-de-Filipe.local>
2013-12-11 09:15:14 -0200

    Implementação da classe Seat, possui identificador de 2 caracteres (linha+coluna do assento) e status do tipo bool como propriedades. No método init que recebe um seatId por parâmetro inicializa status com true ou seja, como livre.

--- 6 - Airline Tickets (CRPA)/CRPA/Models/CRPSeat.m
+++ 6 - Airline Tickets (CRPA)/CRPA/Models/CRPSeat.m
@@ -10,4 +10,23 @@
 
 @implementation CRPSeat
 
+#warning ADICIONAR VALIDAÇÃO ALFANUMERICA PARA IDENTIFICADOR DE ASSENTO.
+- (id)initWithId:(NSString *)seatId
+{
+    if (self == nil) {
+        if (seatId.length != 2) {
+            NSLog(@"O tamanho é %ld inválido para nome do Aeroporto. Deve ser exatamente 2 caracteres.", seatId.length);
+            return nil;
+        }
+        
+        _seatId = seatId;
+        _seatStatus = true;
+        
+        return self;
+    }
+    
+    return nil;
+}
+
+
 @end
