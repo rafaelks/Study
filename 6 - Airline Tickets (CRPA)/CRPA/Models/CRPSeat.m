c41add6ca4e1d3fed79aba9d7034fd0440fd34c8
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2013-12-11 11:26:57 -0200

    Adustments on Seat

--- 6 - Airline Tickets (CRPA)/CRPA/Models/CRPSeat.m
+++ 6 - Airline Tickets (CRPA)/CRPA/Models/CRPSeat.m
@@ -10,15 +10,9 @@
 
 @implementation CRPSeat
 
-#warning ADICIONAR VALIDAÇÃO ALFANUMERICA PARA IDENTIFICADOR DE ASSENTO.
 - (id)initWithId:(NSString *)seatId
 {
-    if (self == nil) {
-        if (seatId.length != 2) {
-            NSLog(@"O tamanho é %ld inválido para nome do Aeroporto. Deve ser exatamente 2 caracteres.", seatId.length);
-            return nil;
-        }
-        
+    if (self) {
         _seatId = seatId;
         _seatStatus = true;
         
