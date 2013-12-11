0bde4476a55e7807deb235984f1856dff052c6bc
Gabriel Tondin <tondingabriel@gmail.com>
2013-12-11 10:57:39 -0200

    init method section class

--- 6 - Airline Tickets (CRPA)/CRPA/CRPSection.h
+++ 6 - Airline Tickets (CRPA)/CRPA/CRPSection.h
@@ -9,12 +9,15 @@
 #import <Foundation/Foundation.h>
 #import "CRPAirline.h"
 #import "CRPFlight.h"
+#import "CRPSeat.h"
 
 @interface CRPSection : NSObject
 
 @property (nonatomic) NSInteger rows;
 @property (nonatomic) NSInteger cols;
+@property (nonatomic, strong) NSArray *seats;
 
--(id)initWithAirline:(CRPAirline *)airline withFlight:(CRPFlight *)flight withRows:(NSInteger)rows withCols:(NSInteger)cols;
+-(id)initWithAirline:(CRPAirline *)airline withFlight:(CRPFlight *)flID withRows:(NSInteger)rows withCols:(NSInteger)cols;
+
 
 @end
