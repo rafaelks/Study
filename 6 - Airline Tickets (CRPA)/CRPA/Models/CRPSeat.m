89286f2333126af5607b38e1c936e87ed24808cb
Filipe Alvarenga <filipealvarenga@MacBook-Pro-de-Filipe-2.local>
2013-12-11 23:12:19 -0200

    adicionada validação de tamanho para seatId

--- 6 - Airline Tickets (CRPA)/CRPA/Models/CRPSeat.m
+++ 6 - Airline Tickets (CRPA)/CRPA/Models/CRPSeat.m
@@ -15,6 +15,12 @@
     self = [super init];
 
     if (self) {
+        
+        if (seatId.length > 4) {
+            NSLog(@"O tamanho %i é inválido para identificador de assento, o tamanho deve ser de até 4 caracters.", (int) [seatId length]);
+            return nil;
+        }
+        
         _seatId = seatId;
         _seatStatus = true;
         
