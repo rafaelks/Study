87e171c538e430e7c6a354f4896bf9354fe18f12
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2014-01-21 09:43:50 -0200

    Finalize ZoomImages project

--- ZoomImages/ZoomImages/RKSImageViewController.m
+++ ZoomImages/ZoomImages/RKSImageViewController.m
@@ -10,7 +10,10 @@
 
 @interface RKSImageViewController ()
 
+@property (nonatomic, weak) IBOutlet UIScrollView *scrollView;
 @property (nonatomic, weak) IBOutlet UIImageView *imageView;
+@property (nonatomic, weak) IBOutlet UISlider *sliderZoom;
+@property (nonatomic, weak) IBOutlet UISwitch *switchZoomEnabled;
 
 @end
 
@@ -20,6 +23,40 @@
 {
     [self setTitle:[self.image objectForKey:@"title"]];
     [self.imageView setImage:[UIImage imageNamed:[self.image objectForKey:@"image"]]];
+}
+
+#pragma mark - Components actions
+
+- (IBAction)switchZoomEnabledValueChanged:(UISwitch *)sender {
+    if (_switchZoomEnabled.on) {
+        [_scrollView setZoomScale:1.0 animated:YES];
+        [_scrollView setMinimumZoomScale:1.0];
+        [_scrollView setMaximumZoomScale:3.0];
+        [_scrollView setScrollEnabled:YES];
+        [_sliderZoom setEnabled:YES];
+    } else {
+        [_scrollView setZoomScale:1.0 animated:YES];
+        [_scrollView setMinimumZoomScale:1.0];
+        [_scrollView setMaximumZoomScale:1.0];
+        [_scrollView setScrollEnabled:NO];
+        [_sliderZoom setEnabled:NO];
+    }
+}
+
+- (IBAction)sliderZoomScaleValueChanged:(UISlider *)sender {
+    [_scrollView setZoomScale:_sliderZoom.value animated:YES];
+}
+
+#pragma mark - ScrollView delegates
+
+- (void)scrollViewDidZoom:(UIScrollView *)scrollView
+{
+    [_sliderZoom setValue:_scrollView.zoomScale animated:YES];
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return _imageView;
 }
 
 @end
